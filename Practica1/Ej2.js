0211597c5d520318428ca82d733b03f17f942151
Ademar Yoel Quispe Torrez <aquispet4@fcpn.edu.bo>
2025-09-21 13:34:57 -0400

    Add files via upload

---
+++ Practica1/Ej2.js
@@ -0,0 +1,7 @@
+//Crear una funcion flecha que calcule el cuadrado de un numero
+
+const cuadrado = (x) => {
+    return x*x;
+}
+
+console.log(cuadrado(6));
---
+++ Practica1/Ej3.js
@@ -0,0 +1,7 @@
+//Pedir un numero y mostrar su tabla de multiplicar del 1 al 10
+
+let num = 5;
+
+for(let i = 1; i <= 10; i++) {
+    console.log(num * i);
+}
